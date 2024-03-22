707b7944546044b86641a9122f1982c23391d414
KidQuant <38800128+KidQuant@users.noreply.github.com>
2024-03-21 21:44:19 -0400

    updates

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -6,12 +6,14 @@ return
         open_mapping = [[<c-\>]],
         shade_filetypes = {},
         hide_numbers = true,
+        shading_factor = 2,
         direction = "float",
+        insert_mappings = true,
         close_on_exit = true,
         shell = vim.o.shell,
         float_opt = {
             border = "curved",
-        }
+        },
     },
     config = function(_, opts)
         require('toggleterm').setup(opts)
