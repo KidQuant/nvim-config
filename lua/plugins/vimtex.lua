c47f3bce995a556167a0799ca7c8110357686574
KidQuant <38800128+KidQuant@users.noreply.github.com>
2024-03-28 01:43:11 -0400

    updates to lua files

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -11,5 +11,5 @@ return {
         vim.g.tex_comment_nospell = 1
         vim.g.vimtex_compiler_progname = 'nvr'
         vim.g.vimtex_view_general_options = [[--unique file:@pdf\#src:@line@tex]]
-    end
+    end,
 }
