50904cf15a289e42d9e5ffae46fecd4f9a6d43ea
KidQuant <38800128+KidQuant@users.noreply.github.com>
2024-04-12 13:33:37 -0400

    testing the new formatting config

--- lua/plugins/barbecue-nvim.lua
+++ lua/plugins/barbecue-nvim.lua
@@ -1,17 +1,17 @@
 -- Display LSP-based breadcrumbs
 return {
-    -- https://github.com/utilyre/barbecue.nvim
-    "utilyre/barbecue.nvim",
-    name = "barbecue",
-    version = "*",
-    theme = "tokyonight",
-    dependencies = {
-        -- https://github.com/SmiteshP/nvim-navic
-        "SmiteshP/nvim-navic",
-        -- https://github.com/nvim-tree/nvim-web-devicons
-        "nvim-tree/nvim-web-devicons", -- optional dependency
-    },
-    opts = {
-        -- configurations go here
-    },
+	-- https://github.com/utilyre/barbecue.nvim
+	"utilyre/barbecue.nvim",
+	name = "barbecue",
+	version = "*",
+	theme = "tokyonight",
+	dependencies = {
+		-- https://github.com/SmiteshP/nvim-navic
+		"SmiteshP/nvim-navic",
+		-- https://github.com/nvim-tree/nvim-web-devicons
+		"nvim-tree/nvim-web-devicons", -- optional dependency
+	},
+	opts = {
+		-- configurations go here
+	},
 }
