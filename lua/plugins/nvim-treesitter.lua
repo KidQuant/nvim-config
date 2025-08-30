d90658f38c92129c1e512db71c289911147decc2
KidQuant <38800128+KidQuant@users.noreply.github.com>
2025-08-30 00:12:53 -0400

    :q

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,31 +1,54 @@
 -- Code Tree Support / Syntax Highlighting
 return {
-	-- https://github.com/nvim-treesitter/nvim-treesitter
-	"nvim-treesitter/nvim-treesitter",
-	event = "VeryLazy",
-	dependencies = {
-		-- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-		"nvim-treesitter/nvim-treesitter-textobjects",
-	},
-	build = ":TSUpdate",
-	opts = {
-		highlight = {
-			enable = true,
-		},
-		indent = { enable = true },
-		auto_install = true, -- automatically install syntax support when entering new file type buffer
-		ensure_installed = {
-			"lua",
-			"python",
-			"bash",
-			"markdown",
-			"sql",
-			"cpp",
-			"markdown_inline",
-		},
-	},
-	config = function(_, opts)
-		local configs = require("nvim-treesitter.configs")
-		configs.setup(opts)
-	end,
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    config = function()
+        -- import nvim-treesitter plugin
+        local treesitter = require("nvim-treesitter.configs")
+
+        -- configure treesitter
+        treesitter.setup({ -- enable syntax highlighting
+            highlight = {
+                enable = true,
+            },
+            -- enable indentation
+            indent = { enable = true },
+            -- ensure these language parsers are installed
+            ensure_installed = {
+                "json",
+                "javascript",
+                "typescript",
+                "tsx",
+                "yaml",
+                "html",
+                "css",
+                "prisma",
+                "markdown",
+                "markdown_inline",
+                "svelte",
+                "graphql",
+                "bash",
+                "lua",
+                "vim",
+                "dockerfile",
+                "gitignore",
+                "query",
+                "vimdoc",
+                "c",
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+        })
+
+        -- use bash parser for zsh files
+        vim.treesitter.language.register("bash", "zsh")
+    end,
 }
